901c0f6ca9c824d6cfc3c82efc1ead1d2feb97ee
iamarjun <arjunmanoj1995@gmail.com>
2023-09-05 19:19:33 +0530

    Setup middleware for /metrics endpoint

--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	cfg := apiConfig{
+		fileserverHits: 0,
+	}
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+
+	corsMux := middlewareCors(mux)
+
+	httpServer := &http.Server{
+		ReadTimeout: http.DefaultClient.Timeout,
+		Handler:     corsMux,
+		Addr:        ":8080",
+	}
+
+	httpServer.ListenAndServe()
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	type metrics struct {
+		hits int
+	}
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Before: %v", cfg.fileserverHits)
+		cfg.fileserverHits++
+		log.Printf("After: %v", cfg.fileserverHits)
+		next.ServeHTTP(w, r)
+	})
 }
