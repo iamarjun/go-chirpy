efcac43a55c6b748bad371e8ec4720982c2e33e9
iamarjun <arjunmanoj1995@gmail.com>
2023-09-11 12:23:41 +0530

    Update get chirp endpoint to handle query params

--- handler_get_chirps.go
+++ handler_get_chirps.go
@@ -15,61 +15,27 @@ import (
 )
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request, db *database.DB) {
-	// authorizationToken := r.Header.Get("Authorization")
 
-	// if len(authorizationToken) == 0 {
-	// 	respondWithError(w, 401, fmt.Sprintln("jwt token not found"))
-	// 	return
-	// }
+	authorId, err := strconv.Atoi(r.URL.Query().Get("author_id"))
 
-	// splitAuth := strings.Split(authorizationToken, " ")
+	fmt.Printf("Author ID: %v\n", authorId)
 
-	// if len(splitAuth) == 1 {
-	// 	respondWithError(w, 401, fmt.Sprintln("invalid jwt token"))
-	// 	return
-	// }
+	if err != nil {
+		respondWithError(w, 400, err.Error())
+		return
+	}
 
-	// jwtToken := splitAuth[1]
+	if authorId > 0 {
+		chirp, err := db.GetChirpsByAuthorId(authorId)
 
-	// token, err := jwt.ParseWithClaims(jwtToken, &cfg.accessJwtClaims, func(t *jwt.Token) (interface{}, error) {
-	// 	return []byte(cfg.jwtSecret), nil
-	// }, jwt.WithLeeway(2*time.Second))
+		if err != nil {
+			respondWithError(w, 400, err.Error())
+			return
+		}
 
-	// if err != nil {
-	// 	respondWithError(w, 401, fmt.Sprintf(" %v", err))
-	// 	return
-	// }
-
-	// if !token.Valid {
-	// 	respondWithError(w, 401, "invalid token")
-	// 	return
-	// }
-
-	// issuer, err := token.Claims.GetIssuer()
-
-	// if err != nil {
-	// 	respondWithError(w, 401, fmt.Sprintf(" %v", err))
-	// 	return
-	// }
-
-	// if issuer != ACCESS_ISSUER {
-	// 	respondWithError(w, 401, "invalid token")
-	// 	return
-	// }
-
-	// id, err := token.Claims.GetSubject()
-
-	// if err != nil {
-	// 	respondWithError(w, 401, fmt.Sprintf(" %v", err))
-	// 	return
-	// }
-
-	// userID, err := strconv.Atoi(id)
-
-	// if err != nil {
-	// 	respondWithError(w, 401, fmt.Sprintf(" %v", err))
-	// 	return
-	// }
+		respondWithJson(w, 200, chirp)
+		return
+	}
 
 	chirps, err := db.GetChirps()
 
